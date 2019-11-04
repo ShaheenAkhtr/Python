bbc4c8d5201774ef55a77503aa74fef78eb1eecf
ShaheenAkhtr <57349295+ShaheenAkhtr@users.noreply.github.com>
2019-11-04 19:12:32 +0500

    Assignment 1

---
+++ Assignment1.py
@@ -0,0 +1,47 @@
+print("**********************************************************************************************")
+print("1. Write a Python program to print the following string in a specific format (see the output).")
+print("**********************************************************************************************")
+var1 = "Twinkle, twinkle, little star,\n\tHow I wonder what you are!\n\t\tUp above the world so high,\n\t\tLike a " \
+       "diamond in the sky.\nTwinkle, twinkle, little star,\n\tHow I wonder what you are "
+print(var1)
+
+print("")
+print("*****************************************************************")
+print("2. Write a Python program to get the Python version you are using")
+print("*****************************************************************")
+python -v
+
+print("")
+print("***************************************************************")
+print("3. Write a Python program to display the current date and time.")
+print("***************************************************************")
+import datetime as dt
+print("Today's Date: " + str(dt.date.today()))
+print("Time: " + str(dt.datetime.now().time()))
+
+print("")
+print("***************************************************************")
+print("4. Write a Python program which accepts the radius of a circle from the user and compute the area.")
+print("***************************************************************")
+import math as mt
+CircleRadius = int(input("Input Radius of circle: "))
+AreaOfCircle = mt.pi * (CircleRadius ** 2)
+print("Area Of Circle: " + str(AreaOfCircle))
+
+print("")
+print("***************************************************************")
+print("5. Write a Python program which accepts the user's first and last name and print them in reverse order with a "
+      "space between them.")
+print("***************************************************************")
+FirstName = input("Input First Name: ")
+LastName = input("Input Last Name: ")
+print(FirstName[::-1] + " " + LastName[::-1])
+
+print("")
+print("**********************************************************************************")
+print("6. Write a python program which takes two inputs from user and print them addition")
+print("**********************************************************************************")
+FirstNumber = int(input("Input 1st Number: "))
+SecondNumber = int(input("Input 2nd Number: "))
+print(FirstNumber + SecondNumber)
+
